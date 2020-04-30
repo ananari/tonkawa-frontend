980a953a73569967dfa0124694234226a4571f69
Anais Ahmed <anaismahmed@uchicago.edu>
2020-04-30 12:44:34 -0500

    testing showing the search results

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 
 export default class SearchResults extends Component {
 
+  componentDidUpdate(){
+    console.log(`results compoment has props of ${this.state}`)
+  }
+
   render(){
     if(this.props.results.length > 0){
       return(
