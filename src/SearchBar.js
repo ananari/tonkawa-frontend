61d0072fe1a3ca0bcda43f60de5b80ade77ca65f
Anais Ahmed <anaismahmed@uchicago.edu>
2020-04-30 09:40:28 -0500

    added searchbar

---
+++ src/SearchBar.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+export default class SearchBar extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      query: ""
+    };
+  }
+
+  handleChange = event => {
+    event.persist();
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log("you did it!")
+  }
+
+
+  render(){
+    return(
+      <div className="searchbar">
+        <form onSubmit={event => this.handleSubmit(event)}>
+            <div>
+              <label>Search term </label>
+              <input type="text" name="query" onChange={event => this.handleChange(event)} value={this.state.query} placeholder="Search here" />
+              <input type="submit" value="Search" />
+            </div>
+        </form>
+      </div>
+    )
+  }
+}
